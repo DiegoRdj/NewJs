0660cd92ad14d758f3ef3cbd6db7d61f979ec523
DIego R <diegorolimdejesus@gmail.com>
2026-05-28 16:53:03 -0300

    bump

--- CursoJavaScript_PARTE_01/Aula_01/app.js
+++ CursoJavaScript_PARTE_01/Aula_01/app.js
@@ -1,1 +1,5 @@
-console.log("Hello  World!");
+console.log("Hello  'World!'");
+console.log('"Hello  World!"');
+console.log(`Hello  World!`);
+console.log(123456);
+console.log(25, 14, 31, "PERSONA");
---
+++ CursoJavaScript_PARTE_01/Aula_02_Exercicio/Exercicio.js
@@ -0,0 +1,2 @@
+console.log(`Meu nome é "PERSONA".` , "Estou aprendendo JavaScript às", 16, "da Tarde.")
+console.log('Meu nome é "PERSONA". Estou aprendendo js as', 10, "da manha"); // Jeito dele
