f8a8184ea669e1db197a5c3ecead52738b200282
DIego R <diegorolimdejesus@gmail.com>
2026-05-29 10:56:16 -0300

    var

---
+++ CursoJavaScript_PARTE_01/Aula_05_Variaveis_Com_Let/Variaveis.js
@@ -0,0 +1,48 @@
+/*let nome = "Luiz";
+
+console.log( nome, "nasceu em 84");
+console.log("em 2000", nome, "conheceu maria");
+console.log(nome, "casou com maria");
+console.log(nome, "teve 1 filho");
+console.log(nome, " tem um filho chamado duardo");
+
+*/
+
+/* let nome; // Declarou a variavel
+nome = 'Joker' // Inicializando a Variavel
+console.log(nome);
+nome = 'Makoto';
+console.log(nome)
+
+// Let nome; PROIBIDO POIS JA FOI EXECUTADO
+*/
+
+// Não PODEMOS usar PALAVRAS RESERVADAS
+
+/**
+ * let
+ * if
+ * console
+ * etc
+ */
+
+//Variaveis precisam ter nomes Significativos!!!
+
+let nomeCliente = "Joao";
+console.log(nomeCliente);
+
+//Não pode começar com o numero
+// Não podem conter espaços ou traços
+// nem traços, somente _
+//Utilizamdos camelCase -> nomeStaff
+
+let nomeCompleto = 'Makoto yuki'
+console.log(nomeCompleto)
+
+//case-Sensitive
+let nome  = "Yu"
+let Nome  = "Kotone"
+console.log(nome);
+console.log(Nome);
+
+//Não podemos redeclarar variaveis com let
---
+++ CursoJavaScript_PARTE_01/Aula_06_Variaveis_Com_Const/Variaveis.js
@@ -0,0 +1,22 @@
+//As regras sao as msm
+// ELA PRECISA SER CRIADA e INCIALIZADA
+
+//const nome = "Makoto";
+//console.log(nome)
+
+//nome = "Yu"; Não pode redefinir
+
+    // + - * /
+
+    // String = text | Number = numero
+const primeiroNumero = 5; // Number
+const segundoNumero = "10";
+const conta = primeiroNumero * segundoNumero
+const contaDuplicado = conta * 2 
+let resultadoTriplicado = conta * 3;
+resultadoTriplicado = resultadoTriplicado + 5
+console.log(resultadoTriplicado);
+
+console.log(typeof(primeiroNumero)) // Qual é o tipo da variavel do "primeiroNumero"
+console.log(typeof(segundoNumero)) // Qual é o tipo da variavel do "segundoNumero"
+
