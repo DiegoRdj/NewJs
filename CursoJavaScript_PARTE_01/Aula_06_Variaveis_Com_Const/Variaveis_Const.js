117598c71be7473907bdfbacd4dadf69929ab426
DIego R <diegorolimdejesus@gmail.com>
2026-05-29 11:19:53 -0300

    ex_de_var

--- CursoJavaScript_PARTE_01/Aula_06_Variaveis_Com_Const/Variaveis_Const.js
+++ CursoJavaScript_PARTE_01/Aula_06_Variaveis_Com_Const/Variaveis_Const.js
@@ -19,4 +19,5 @@ console.log(resultadoTriplicado);
 
 console.log(typeof(primeiroNumero)) // Qual é o tipo da variavel do "primeiroNumero"
 console.log(typeof(segundoNumero)) // Qual é o tipo da variavel do "segundoNumero"
+console.log(typeof(primeiroNumero + segundoNumero)) // Qual é o tipo da variavel se juntar os 2
 
---
+++ CursoJavaScript_PARTE_01/Aula_07_Ex_Com_Variaveis/Ex_Variaveis.js
@@ -0,0 +1,13 @@
+const nome = "Makoto"
+const sobrenome = "Yuki"
+const idade = 16;
+const peso = 60;
+const altura = 1.70
+let imc;
+imc = peso / (altura * altura);
+let anoNascimento;
+anoNascimento = 1992
+
+console.log(nome, sobrenome, "tem", idade, "anos,","pesa",peso,"kg")
+console.log("Tem",altura,"de altura e seu imc é de",imc)
+console.log(nome,sobrenome, "nasceu em",anoNascimento)
