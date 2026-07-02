453be2e677387d70f202bc1ac37b5b94de326c1c
DIego R <diegorolimdejesus@gmail.com>
2026-07-02 18:32:15 -0300

    acabei

---
+++ CursoJavaScript_PARTE_02/Aula_27_Exercicio_Timer/assets/js/main.js
@@ -0,0 +1,21 @@
+const relogio = document.querySelector('.relogio')
+const iniciar = document.querySelector('.iniciar')
+const pausar = document.querySelector('.pausar')
+const zerar = document.querySelector('.zerar')
+
+let data = new Date();
+let dataInicial;
+
+
+iniciar.addEventListener('click', function(event){
+    
+});
+
+pausar.addEventListener('click', function(event){
+   
+});
+
+zerar.addEventListener('click', function(event){
+   
+});
+
